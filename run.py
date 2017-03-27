6f5602fe30fd98bbe264edc5e77b19aab84084bc
Yair <yair.horesh@gmail.com>
2017-03-27 19:45:02 +0300

    wip

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-#!flask/bin/python
-import app
-app.app.run(debug=True)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
